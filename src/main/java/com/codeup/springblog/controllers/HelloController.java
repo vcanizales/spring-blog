f9c877f4f42f4e9d4a5b4326bc56b834e862236f
Valeria Canizales <vcanizales93@gmail.com>
2022-11-15 13:37:04 -0600

    initialize repo

---
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloWorld(){
+        return "<h1>Hello World</h1>";
+    }
+}
