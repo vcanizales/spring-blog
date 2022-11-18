9a2e3982210d3096cfcc619da803e8b3592aacad
Valeria Canizales <vcanizales93@gmail.com>
2022-11-18 11:08:04 -0600

    Posts exercise views

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,22 +1,41 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
 
-    @GetMapping
+
+
+    @GetMapping("/")
     @ResponseBody
     public String posts(){
         return "These are all of the posts.";
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable String id){
-        return "Your ID is " + id + ".";
+//    @ResponseBody
+    public String individualPost(@PathVariable long id, Model model){
+//        return "Your ID is " + id + ".";
+        Post post = new Post(1,"title", "Here is my title");
+        model.addAttribute("post", post);
+        return "posts/show";
+    }
+
+    @GetMapping
+    public String postsIndex(Model model){
+        ArrayList<Post> postIndex = new ArrayList<Post>();
+        postIndex.add(new Post(1,"Title 1", "Body 1"));
+        postIndex.add(new Post(2,"Title 2", "Body 2"));
+        model.addAttribute("posts", postIndex);
+
+        return "posts/index";
     }
 
     @GetMapping("/create")
